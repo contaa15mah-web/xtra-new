8015355103e5cb901c84737c25bcd86b98d96137
contaa15mah-web <contaa15.mah@gmail.com>
2026-02-05 21:31:11 -0300

    Update browse_page.dart

--- lib/presentation/pages/browse_page.dart
+++ lib/presentation/pages/browse_page.dart
@@ -1,1 +1,12 @@
+import 'package:flutter/material.dart';
 
+class BrowsePage extends StatelessWidget {
+  const BrowsePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('Browse')),
+    );
+  }
+}
