20c445e438eee732a47a2a3cda9d88df165954d0
contaa15mah-web <contaa15.mah@gmail.com>
2026-02-05 22:36:33 -0300

    Update video_player_widget.dart

--- lib/presentation/widgets/video_player_widget.dart
+++ lib/presentation/widgets/video_player_widget.dart
@@ -1,6 +1,6 @@
 // lib/presentation/widgets/video_player_widget.dart
 
-import 'package:better_player/better_player.dart';
+// import 'package:better_player/better_player.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:xtra_neo/core/theme/app_theme.dart';
